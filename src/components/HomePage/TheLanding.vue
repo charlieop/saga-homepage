6cc2833524a79653a97101e45eb2107508de38ac
charlieop <spchanae@connect.ust.hk>
2023-11-01 17:25:56 +0800

    modified common css properties and added data-banner page

---
+++ src/components/HomePage/TheLanding.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="landing">
+    <h1>欢迎来到SAGA!</h1>
+    <p>这将是网站的落地页, 目前正在进行设计中...</p>
+    <p>敬请期待</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.landing {
+  text-align: center;
+  padding: 10rem;
+
+
+  min-height: 100svh;
+}
+
+</style>
+
---
+++ src/components/HomePage/TheNameIntro.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="name-intro">
+    <h1>haha</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.name-intro {
+    /* remove after done */
+    min-height: 100svh;
+    margin-bottom: 50svh;
+}
+.name-intro {
+  background-color: var(--clr-background-muted);
+  border-radius: 0 0 10vw 10vw;
+  padding: 5rem var(--page-padding-inline);
+}
+</style>
