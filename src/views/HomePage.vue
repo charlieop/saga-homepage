1f9127546b8fab790e9c91e75fcd408a9668ce77
charlieop <spchanae@connect.ust.hk>
2023-11-04 14:38:42 +0800

    added footer

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,20 @@
 <template>
-  <TheMainNavigation></TheMainNavigation>
+  <MainNavigation></MainNavigation>
   <TheLanding></TheLanding>
   <TheDataBanner></TheDataBanner>
   <TheNameIntro></TheNameIntro>
   <div class="main__add-blank"></div>
   <TheValues></TheValues>
+  <Footer></Footer>
 </template>
 
 <script setup>
-import TheMainNavigation from "@/components/HomePage/TheMainNavigation.vue";
+import MainNavigation from "@/components/MainNavigation.vue";
 import TheLanding from "@/components/HomePage/TheLanding.vue";
 import TheDataBanner from "@/components/HomePage/TheDataBanner.vue";
 import TheNameIntro from "@/components/HomePage/TheNameIntro.vue";
 import TheValues from "@/components/HomePage/TheValues.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <style scoped>
