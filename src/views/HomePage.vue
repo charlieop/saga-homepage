091462b1f28b91e6d679878a6083cf052790a7d1
Jade0120 <118807084+Jade0120@users.noreply.github.com>
2023-11-05 20:42:53 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,6 +5,10 @@
   <TheNameIntro></TheNameIntro>
   <div class="main__add-blank"></div>
   <TheValues></TheValues>
+  <div class="learn-more">
+    <TheLearnMore imageUrl="/src/assets/imgs/lotus.jpg" title="志愿者报名" detail="Work with us!"></TheLearnMore>
+    <TheLearnMore imageUrl="/src/assets/imgs/rose.jpg" title="学生报名" detail="来体验我们的课程!"></TheLearnMore>
+  </div>
   <Footer></Footer>
 </template>
 
@@ -14,6 +18,7 @@ import TheLanding from "@/components/HomePage/TheLanding.vue";
 import TheDataBanner from "@/components/HomePage/TheDataBanner.vue";
 import TheNameIntro from "@/components/HomePage/TheNameIntro.vue";
 import TheValues from "@/components/HomePage/TheValues.vue";
+import TheLearnMore from "@/components/HomePage/TheLearnMore.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 
