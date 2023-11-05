4b3a3f19de55b9dd2f74a913e01709d97fa966bf
Jade0120 <118807084+Jade0120@users.noreply.github.com>
2023-11-05 17:33:04 +0800

    Add files via upload

---
+++ src/components/HomePage/TheContact.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="contact">
+        <div class="left">
+            <div class="left-title">联系我们 Contact us</div>
+            <div class="left-saga">详情 For more details</div>
+        </div>
+        <div class="right">
+            <div class="right-title">联系方式 Contact information</div>
+            <div class="way">Email：external-affairs@saga-xingguang.com</div>
+            <div class="way">Wechat: 13730835212(手机与微信同号)</div>
+            <div class="way"> WeChat official account (微信公众号)：星光SAGA</div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style>
+.contact{
+    background-color: var(--clr-background-muted);
+    height: 12rem;
+    color: var(--clr-text-muted);
+    display: flex;
+    justify-content: center;
+    padding: 1.5rem 0;
+}
+.left{
+    margin-right: 10rem;
+}
+.left-title{
+    color: var( --clr-text);
+    font-size: 3rem;
+    margin-bottom: 1.2rem;
+}
+.left-saga{
+    width: 15rem;
+    height: 3.5rem;
+    background-color: var(--clr-text-muted);
+    font-size: 1.5rem;
+    line-height: 3.2rem;
+    text-align: center;
+    border-radius: 2.1rem;
+    color: var(--clr-text);
+}
+.right{
+    padding-left: 1.3rem;
+    color:  var(--clr-text);
+}
+.right-title{
+    padding-top: 0.8rem;
+    font-size: 1.2rem;
+    padding-bottom: 1rem;
+}
+.way{
+    margin-bottom: 0.3rem;
+}
+</style>
+  
+  
