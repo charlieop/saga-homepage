5b31b5bc2e2ba708642f7f4b16bd9ac8f9ca97ed
charlieop <spchanae@connect.ust.hk>
2023-11-11 20:06:43 +0800

    modified router config

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import HomePage from "@/views/HomePage.vue";
 const routes = [{ path: "/", name: "HomePage", component: HomePage }];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
