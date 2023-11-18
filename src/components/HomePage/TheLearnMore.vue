e22a58cf89319e86d228139ea13a219e1b665760
xueeeeeeeeeeeeeeeeeeeee <122249998+xueeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2023-11-18 22:01:29 +0800

    Update TheLearnMore.vue

--- src/components/HomePage/TheLearnMore.vue
+++ src/components/HomePage/TheLearnMore.vue
@@ -47,7 +47,7 @@ imgUrl.value = props.imageUrl;
   position: relative;
 }
 .title {
-  /* font-size: 2rem; */
+ font-size:var(--fs-600);
   color: var(--clr-text);
   font-weight: 600;
   text-align: right;
@@ -86,7 +86,7 @@ imgUrl.value = props.imageUrl;
   top: 70%;
   left: 3.2rem;
   background-color: var(--clr-secondary);
-  font-size: 1.5rem;
+   font-size: var(--fs-400);
   width: 10rem;
   height: 3rem;
   border-radius: 0.33rem;
