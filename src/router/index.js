86e9e0a1ab1b3d9af9075c23c6ddb6064222665c
charlieop <spchanae@connect.ust.hk>
2023-12-04 18:04:13 +0800

    changed base

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ const routes = [{ path: "/", name: "HomePage", component: HomePage }];
 
 const router = createRouter({
   // replace with this when run npm run build
-  // history: createWebHistory(process.env.BASE_URL),
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(),
   routes,
 });
 
