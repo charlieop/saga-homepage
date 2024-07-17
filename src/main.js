bde469f50297f22f85f0f3bcea447a0a3d8da42e
charlieop <spchanae@connect.ust.hk>
2024-07-17 21:28:07 +0800

    created half of the form

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import "@/assets/css/fonts.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vueform from "@vueform/vueform";
+import vueformConfig from "./../vueform.config";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(Vueform, vueformConfig).mount("#app");
--- src/views/VolunteersApplicationPage.vue
+++ src/views/VolunteersApplicationPage.vue
@@ -1,28 +1,547 @@
 <template>
-    <MainNavigation></MainNavigation>
-    <section class="volinteers-application">
-        <h1>Volunteers Application Page</h1>
-    </section>
-    <Footer></Footer>
+  <!-- <MainNavigation></MainNavigation> -->
+  <section class="volinteers-application">
+    <Vueform
+      :display-errors="false"
+      validate-on="step"
+      :force-labels="false"
+      :float-placeholders="true"
+      add-class="vf-volunteer-application"
+    >
+      <template #empty>
+        <FormSteps>
+          <FormStep
+            name="page0"
+            label="Step 0"
+            :elements="[
+              'title',
+              'subtitle',
+              'info',
+              'info__text1',
+              'info__text2',
+              'p',
+              'h3',
+            ]"
+          />
+          <FormStep
+            name="page1"
+            label="Step 1"
+            :elements="['basic_info', 'name', 'email', 'phone', 'sex']"
+          />
+          <FormStep
+            name="page2"
+            label="Step 2"
+            :elements="['academic_info', 'school', 'major', 'grade']"
+          />
+          <FormStep name="page3" label="Step 3" :elements="[]" />
+          <FormStep name="page4" label="Step 4" :elements="[]" />
+        </FormSteps>
 
+        <FormElements>
+          <StaticElement
+            name="title"
+            tag="h1"
+            align="left"
+            content="SAGA 星光 · 第五期 | 志愿者招募邀请"
+            top="1"
+          />
+          <StaticElement
+            name="subtitle"
+            tag="p"
+            content="感谢你对SAGA星光的兴趣与关注, 我们非常期待你的加入!"
+            align="right"
+            size="sm"
+            bottom="3"
+          />
+          <StaticElement name="info" tag="h2" content="志愿者须知:" />
+          <StaticElement
+            name="info__text1"
+            tag="p"
+            content="本次志愿者招募的 <strong style='font-weight: bold;'>截止日期为: 2024年XX月XX日 23:59分</strong>, 招募为滚动制, 招募完成的部门会停止接受新的申请. "
+          />
+          <StaticElement
+            name="info__text2"
+            tag="p"
+            content="SAGA 星光非常重视你的隐私. 我们保证在未经你同意的情况下不会向任何第三方透露你在申请中提交的信息."
+          />
+          <StaticElement
+            name="p"
+            tag="p"
+            content="如果你在申请的过程中遇到任何问题, 或是有任何疑问, 请随时于 <a href='mailto:support@saga-xingguang.com'>support@saga-xingguang.com</a> 与我们取得联系"
+          />
+          <StaticElement
+            name="h3"
+            tag="h3"
+            align="right"
+            content="让我们开始吧! &nbsp; ↓ &nbsp;"
+          />
+          <StaticElement
+            name="basic_info"
+            tag="h2"
+            top="1"
+            content="基本信息"
+          />
+          <TextElement
+            name="name"
+            label="你的名字是?"
+            placeholder="张三"
+            :floating="false"
+            field-name="姓名"
+            :rules="['required', 'min:2']"
+          />
+          <TextElement
+            name="email"
+            label="你的电子邮箱是?"
+            input-type="email"
+            :rules="['required']"
+            :floating="false"
+            placeholder="person@example.com"
+            field-name="电子邮箱"
+          />
+          <TextElement
+            name="phone"
+            :rules="['required', 'regex:^\\d{11}$']"
+            autocomplete="off"
+            label="你的手机号码是?"
+            placeholder="13912340000"
+            :floating="false"
+            :addons="{
+              before: '+86 ',
+            }"
+            field-name="手机号码"
+          />
+          <SelectElement
+            name="sex"
+            :items="[
+              {
+                value: 'M',
+                label: '男性',
+              },
+              {
+                value: 'F',
+                label: '女性',
+              },
+              {
+                value: 'O',
+                label: '其他/ 不愿透露',
+              },
+            ]"
+            :search="true"
+            :native="false"
+            label="你的姓别是?"
+            input-type="search"
+            :rules="['required']"
+            open-direction="top"
+            field-name="性别"
+          />
+          <StaticElement
+            name="academic_info"
+            tag="h2"
+            top="1"
+            content="学业信息"
+          />
+          <TextElement
+            name="school"
+            label="你就读的院校是?"
+            :floating="false"
+            placeholder="香港科技大学"
+            description="海外大学请填写中文全名, 高三学生请填写: 暂定"
+            info="你目前所就读/ 毕业的高等教育机构"
+            field-name="就读院校"
+            :rules="['required', 'min:2']"
+          />
+          <TextElement
+            name="major"
+            label="你的专业是?"
+            info="本科生请填写专业, 研究生请填写课题方向"
+            placeholder="计算机科学"
+            description="无专业请填写: 暂无"
+            field-name="专业"
+            :floating="false"
+            :rules="['required', 'min:2']"
+          />
+          <SelectElement
+            name="grade"
+            label="你于今年秋季开学时的年级是?"
+            :items="[
+              {
+                value: 'UG1',
+                label: '大一',
+              },
+              {
+                value: 'UG2',
+                label: '大二',
+              },
+              {
+                value: 'UG3',
+                label: '大三',
+              },
+              {
+                value: 'UG4',
+                label: '大四',
+              },
+              {
+                value: 'UGGRAD',
+                label: '本科毕业',
+              },
+              {
+                value: 'PG',
+                label: '硕/博士生',
+              },
+              {
+                value: 'PGGRAD',
+                label: '硕/博士毕业',
+              },
+              {
+                value: 'OTHER',
+                label: '其他',
+              },
+            ]"
+            :native="false"
+            open-direction="top"
+            :rules="['required']"
+            field-name="年级"
+          />
+        </FormElements>
+
+        <FormStepsControls />
+      </template>
+    </Vueform>
+  </section>
+  <!-- <Footer></Footer> -->
 </template>
 
 <script setup>
 import MainNavigation from "@/components/MainNavigation.vue";
 
 import Footer from "@/components/Footer.vue";
-
 </script>
 
 <style scoped>
-
 .volinteers-application {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  padding: 5rem 2rem;
+  min-height: 100vh;
+  background-color: rgb(155, 155, 155);
 }
 
-</style>    
+form {
+  color: var(--clr-text);
+  width: 100%;
+  max-width: 40rem;
+  padding: 2.5rem;
+  border-radius: 1rem;
+  height: min-content;
+  background-color: var(--clr-background);
+  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.3);
+}
 
+.vf-volunteer-application *,
+.vf-volunteer-application *:before,
+.vf-volunteer-application *:after,
+.vf-volunteer-application:root {
+  --vf-primary: #07bf9b;
+  --vf-primary-darker: #06ac8b;
+  --vf-color-on-primary: #ffffff;
+  --vf-danger: #ef4444;
+  --vf-danger-lighter: #fee2e2;
+  --vf-success: #10b981;
+  --vf-success-lighter: #d1fae5;
+  --vf-gray-50: #f9fafb;
+  --vf-gray-100: #f3f4f6;
+  --vf-gray-200: #e5e7eb;
+  --vf-gray-300: #d1d5db;
+  --vf-gray-400: #9ca3af;
+  --vf-gray-500: #6b7280;
+  --vf-gray-600: #4b5563;
+  --vf-gray-700: #374151;
+  --vf-gray-800: #1f2937;
+  --vf-gray-900: #111827;
+  --vf-dark-50: #efefef;
+  --vf-dark-100: #dcdcdc;
+  --vf-dark-200: #bdbdbd;
+  --vf-dark-300: #a0a0a0;
+  --vf-dark-400: #848484;
+  --vf-dark-500: #737373;
+  --vf-dark-600: #393939;
+  --vf-dark-700: #323232;
+  --vf-dark-800: #262626;
+  --vf-dark-900: #191919;
+  --vf-ring-width: 2px;
+  --vf-ring-color: #07bf9b66;
+  --vf-link-color: var(--vf-primary);
+  --vf-link-decoration: inherit;
+  --vf-font-size: 1rem;
+  --vf-font-size-sm: 0.875rem;
+  --vf-font-size-lg: 1rem;
+  --vf-font-size-small: 0.875rem;
+  --vf-font-size-small-sm: 0.8125rem;
+  --vf-font-size-small-lg: 0.875rem;
+  --vf-font-size-h1: 2.125rem;
+  --vf-font-size-h1-sm: 2.125rem;
+  --vf-font-size-h1-lg: 2.125rem;
+  --vf-font-size-h2: 1.875rem;
+  --vf-font-size-h2-sm: 1.875rem;
+  --vf-font-size-h2-lg: 1.875rem;
+  --vf-font-size-h3: 1.5rem;
+  --vf-font-size-h3-sm: 1.5rem;
+  --vf-font-size-h3-lg: 1.5rem;
+  --vf-font-size-h4: 1.25rem;
+  --vf-font-size-h4-sm: 1.25rem;
+  --vf-font-size-h4-lg: 1.25rem;
+  --vf-font-size-h1-mobile: 1.5rem;
+  --vf-font-size-h1-mobile-sm: 1.5rem;
+  --vf-font-size-h1-mobile-lg: 1.5rem;
+  --vf-font-size-h2-mobile: 1.25rem;
+  --vf-font-size-h2-mobile-sm: 1.25rem;
+  --vf-font-size-h2-mobile-lg: 1.25rem;
+  --vf-font-size-h3-mobile: 1.125rem;
+  --vf-font-size-h3-mobile-sm: 1.125rem;
+  --vf-font-size-h3-mobile-lg: 1.125rem;
+  --vf-font-size-h4-mobile: 1rem;
+  --vf-font-size-h4-mobile-sm: 1rem;
+  --vf-font-size-h4-mobile-lg: 1rem;
+  --vf-font-size-blockquote: 1rem;
+  --vf-font-size-blockquote-sm: 0.875rem;
+  --vf-font-size-blockquote-lg: 1rem;
+  --vf-line-height: 1.5rem;
+  --vf-line-height-sm: 1.25rem;
+  --vf-line-height-lg: 1.5rem;
+  --vf-line-height-small: 1.25rem;
+  --vf-line-height-small-sm: 1.125rem;
+  --vf-line-height-small-lg: 1.25rem;
+  --vf-line-height-headings: 1.2;
+  --vf-line-height-headings-sm: 1.2;
+  --vf-line-height-headings-lg: 1.2;
+  --vf-line-height-blockquote: 1.5rem;
+  --vf-line-height-blockquote-sm: 1.25rem;
+  --vf-line-height-blockquote-lg: 1.5rem;
+  --vf-letter-spacing: 0px;
+  --vf-letter-spacing-sm: 0px;
+  --vf-letter-spacing-lg: 0px;
+  --vf-letter-spacing-small: 0px;
+  --vf-letter-spacing-small-sm: 0px;
+  --vf-letter-spacing-small-lg: 0px;
+  --vf-letter-spacing-headings: 0px;
+  --vf-letter-spacing-headings-sm: 0px;
+  --vf-letter-spacing-headings-lg: 0px;
+  --vf-letter-spacing-blockquote: 0px;
+  --vf-letter-spacing-blockquote-sm: 0px;
+  --vf-letter-spacing-blockquote-lg: 0px;
+  --vf-gutter: 1rem;
+  --vf-gutter-sm: 0.5rem;
+  --vf-gutter-lg: 1rem;
+  --vf-min-height-input: 2.375rem;
+  --vf-min-height-input-sm: 2.125rem;
+  --vf-min-height-input-lg: 2.875rem;
+  --vf-py-input: 0.375rem;
+  --vf-py-input-sm: 0.375rem;
+  --vf-py-input-lg: 0.625rem;
+  --vf-px-input: 0.75rem;
+  --vf-px-input-sm: 0.5rem;
+  --vf-px-input-lg: 0.875rem;
+  --vf-py-btn: 0.375rem;
+  --vf-py-btn-sm: 0.375rem;
+  --vf-py-btn-lg: 0.625rem;
+  --vf-px-btn: 0.875rem;
+  --vf-px-btn-sm: 0.75rem;
+  --vf-px-btn-lg: 1.25rem;
+  --vf-py-btn-small: 0.25rem;
+  --vf-py-btn-small-sm: 0.25rem;
+  --vf-py-btn-small-lg: 0.375rem;
+  --vf-px-btn-small: 0.625rem;
+  --vf-px-btn-small-sm: 0.625rem;
+  --vf-px-btn-small-lg: 0.75rem;
+  --vf-py-group-tabs: 0.375rem;
+  --vf-py-group-tabs-sm: 0.375rem;
+  --vf-py-group-tabs-lg: 0.625rem;
+  --vf-px-group-tabs: 0.75rem;
+  --vf-px-group-tabs-sm: 0.5rem;
+  --vf-px-group-tabs-lg: 0.875rem;
+  --vf-py-group-blocks: 0.75rem;
+  --vf-py-group-blocks-sm: 0.625rem;
+  --vf-py-group-blocks-lg: 0.875rem;
+  --vf-px-group-blocks: 1rem;
+  --vf-px-group-blocks-sm: 1rem;
+  --vf-px-group-blocks-lg: 1rem;
+  --vf-py-tag: 0px;
+  --vf-py-tag-sm: 0px;
+  --vf-py-tag-lg: 0px;
+  --vf-px-tag: 0.4375rem;
+  --vf-px-tag-sm: 0.4375rem;
+  --vf-px-tag-lg: 0.4375rem;
+  --vf-py-slider-tooltip: 0.125rem;
+  --vf-py-slider-tooltip-sm: 0.0625rem;
+  --vf-py-slider-tooltip-lg: 0.1875rem;
+  --vf-px-slider-tooltip: 0.375rem;
+  --vf-px-slider-tooltip-sm: 0.3125rem;
+  --vf-px-slider-tooltip-lg: 0.5rem;
+  --vf-py-blockquote: 0.25rem;
+  --vf-py-blockquote-sm: 0.25rem;
+  --vf-py-blockquote-lg: 0.25rem;
+  --vf-px-blockquote: 0.75rem;
+  --vf-px-blockquote-sm: 0.75rem;
+  --vf-px-blockquote-lg: 0.75rem;
+  --vf-py-hr: 0.25rem;
+  --vf-space-addon: 0px;
+  --vf-space-addon-sm: 0px;
+  --vf-space-addon-lg: 0px;
+  --vf-space-checkbox: 0.375rem;
+  --vf-space-checkbox-sm: 0.375rem;
+  --vf-space-checkbox-lg: 0.375rem;
+  --vf-space-tags: 0.1875rem;
+  --vf-space-tags-sm: 0.1875rem;
+  --vf-space-tags-lg: 0.1875rem;
+  --vf-space-static-tag-1: 1rem;
+  --vf-space-static-tag-2: 2rem;
+  --vf-space-static-tag-3: 3rem;
+  --vf-floating-top: 0rem;
+  --vf-floating-top-sm: 0rem;
+  --vf-floating-top-lg: 0.6875rem;
+  --vf-bg-input: #ffffff;
+  --vf-bg-input-hover: #ffffff;
+  --vf-bg-input-focus: #ffffff;
+  --vf-bg-input-danger: #ffffff;
+  --vf-bg-input-success: #ffffff;
+  --vf-bg-checkbox: #ffffff;
+  --vf-bg-checkbox-hover: #ffffff;
+  --vf-bg-checkbox-focus: #ffffff;
+  --vf-bg-checkbox-danger: #ffffff;
+  --vf-bg-checkbox-success: #ffffff;
+  --vf-bg-disabled: var(--vf-gray-200);
+  --vf-bg-selected: #1118270d;
+  --vf-bg-passive: var(--vf-gray-300);
+  --vf-bg-icon: var(--vf-gray-500);
+  --vf-bg-danger: var(--vf-danger-lighter);
+  --vf-bg-success: var(--vf-success-lighter);
+  --vf-bg-tag: var(--vf-primary);
+  --vf-bg-slider-handle: var(--vf-primary);
+  --vf-bg-toggle-handle: #ffffff;
+  --vf-bg-date-head: var(--vf-gray-100);
+  --vf-bg-addon: #ffffff00;
+  --vf-bg-btn: var(--vf-primary);
+  --vf-bg-btn-danger: var(--vf-danger);
+  --vf-bg-btn-secondary: var(--vf-gray-200);
+  --vf-color-input: var(--vf-gray-800);
+  --vf-color-input-hover: var(--vf-gray-800);
+  --vf-color-input-focus: var(--vf-gray-800);
+  --vf-color-input-danger: var(--vf-gray-800);
+  --vf-color-input-success: var(--vf-gray-800);
+  --vf-color-disabled: var(--vf-gray-400);
+  --vf-color-placeholder: var(--vf-gray-300);
+  --vf-color-passive: var(--vf-gray-700);
+  --vf-color-muted: var(--vf-gray-500);
+  --vf-color-floating: var(--vf-gray-500);
+  --vf-color-floating-focus: var(--vf-gray-500);
+  --vf-color-floating-success: var(--vf-gray-500);
+  --vf-color-floating-danger: var(--vf-gray-500);
+  --vf-color-danger: var(--vf-danger);
+  --vf-color-success: var(--vf-success);
+  --vf-color-tag: var(--vf-color-on-primary);
+  --vf-color-addon: var(--vf-gray-800);
+  --vf-color-date-head: var(--vf-gray-700);
+  --vf-color-btn: var(--vf-color-on-primary);
+  --vf-color-btn-danger: #ffffff;
+  --vf-color-btn-secondary: var(--vf-gray-700);
+  --vf-border-color-input: var(--vf-gray-300);
+  --vf-border-color-input-hover: var(--vf-gray-300);
+  --vf-border-color-input-focus: var(--vf-primary);
+  --vf-border-color-input-danger: var(--vf-gray-300);
+  --vf-border-color-input-success: var(--vf-gray-300);
+  --vf-border-color-checkbox: var(--vf-gray-300);
+  --vf-border-color-checkbox-focus: var(--vf-primary);
+  --vf-border-color-checkbox-hover: var(--vf-gray-300);
+  --vf-border-color-checkbox-danger: var(--vf-gray-300);
+  --vf-border-color-checkbox-success: var(--vf-gray-300);
+  --vf-border-color-checked: var(--vf-primary);
+  --vf-border-color-passive: var(--vf-gray-300);
+  --vf-border-color-slider-tooltip: var(--vf-primary);
+  --vf-border-color-tag: var(--vf-primary);
+  --vf-border-color-btn: var(--vf-primary);
+  --vf-border-color-btn-danger: var(--vf-danger);
+  --vf-border-color-btn-secondary: var(--vf-gray-200);
+  --vf-border-color-blockquote: var(--vf-gray-300);
+  --vf-border-color-hr: var(--vf-gray-300);
+  --vf-border-color-signature-hr: var(--vf-gray-300);
+  --vf-border-width-input-t: 1px;
+  --vf-border-width-input-r: 1px;
+  --vf-border-width-input-b: 1px;
+  --vf-border-width-input-l: 1px;
+  --vf-border-width-radio-t: 1px;
+  --vf-border-width-radio-r: 1px;
+  --vf-border-width-radio-b: 1px;
+  --vf-border-width-radio-l: 1px;
+  --vf-border-width-checkbox-t: 1px;
+  --vf-border-width-checkbox-r: 1px;
+  --vf-border-width-checkbox-b: 1px;
+  --vf-border-width-checkbox-l: 1px;
+  --vf-border-width-dropdown: 1px;
+  --vf-border-width-btn: 1px;
+  --vf-border-width-toggle: 0.125rem;
+  --vf-border-width-tag: 1px;
+  --vf-border-width-blockquote: 3px;
+  --vf-shadow-input: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-input-hover: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-input-focus: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-handles: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-handles-hover: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-handles-focus: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-btn: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-shadow-dropdown: 0px 0px 0px 0px rgba(0, 0, 0, 0);
+  --vf-radius-input: 0.25rem;
+  --vf-radius-input-sm: 0.25rem;
+  --vf-radius-input-lg: 0.25rem;
+  --vf-radius-btn: 0.25rem;
+  --vf-radius-btn-sm: 0.25rem;
+  --vf-radius-btn-lg: 0.25rem;
+  --vf-radius-small: 0.25rem;
+  --vf-radius-small-sm: 0.25rem;
+  --vf-radius-small-lg: 0.25rem;
+  --vf-radius-large: 0.25rem;
+  --vf-radius-large-sm: 0.25rem;
+  --vf-radius-large-lg: 0.25rem;
+  --vf-radius-tag: 0.25rem;
+  --vf-radius-tag-sm: 0.25rem;
+  --vf-radius-tag-lg: 0.25rem;
+  --vf-radius-checkbox: 0.25rem;
+  --vf-radius-checkbox-sm: 0.25rem;
+  --vf-radius-checkbox-lg: 0.25rem;
+  --vf-radius-slider: 0.25rem;
+  --vf-radius-slider-sm: 0.25rem;
+  --vf-radius-slider-lg: 0.25rem;
+  --vf-radius-image: 0.25rem;
+  --vf-radius-image-sm: 0.25rem;
+  --vf-radius-image-lg: 0.25rem;
+  --vf-radius-gallery: 0.25rem;
+  --vf-radius-gallery-sm: 0.25rem;
+  --vf-radius-gallery-lg: 0.25rem;
+  --vf-checkbox-size: 1rem;
+  --vf-checkbox-size-sm: 0.875rem;
+  --vf-checkbox-size-lg: 1rem;
+  --vf-gallery-size: 6rem;
+  --vf-gallery-size-sm: 5rem;
+  --vf-gallery-size-lg: 7rem;
+  --vf-toggle-width: 3rem;
+  --vf-toggle-width-sm: 2.75rem;
+  --vf-toggle-width-lg: 3rem;
+  --vf-toggle-height: 1.25rem;
+  --vf-toggle-height-sm: 1rem;
+  --vf-toggle-height-lg: 1.25rem;
+  --vf-slider-height: 0.375rem;
+  --vf-slider-height-sm: 0.3125rem;
+  --vf-slider-height-lg: 0.5rem;
+  --vf-slider-height-vertical: 20rem;
+  --vf-slider-height-vertical-sm: 20rem;
+  --vf-slider-height-vertical-lg: 20rem;
+  --vf-slider-handle-size: 1rem;
+  --vf-slider-handle-size-sm: 0.875rem;
+  --vf-slider-handle-size-lg: 1.25rem;
+  --vf-slider-tooltip-distance: 0.5rem;
+  --vf-slider-tooltip-distance-sm: 0.375rem;
+  --vf-slider-tooltip-distance-lg: 0.5rem;
+  --vf-slider-tooltip-arrow-size: 0.3125rem;
+  --vf-slider-tooltip-arrow-size-sm: 0.3125rem;
+  --vf-slider-tooltip-arrow-size-lg: 0.3125rem;
+}
+</style>
