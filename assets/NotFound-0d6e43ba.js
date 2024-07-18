18808c5373e4eabc389ae90a6ac79ecbedc6f639
charlieop <spchanae@connect.ust.hk>
2024-07-18 17:25:17 +0800

    updated

--- assets/NotFound-0d6e43ba.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,a as n,b as e,l as s}from"./index-97452fc8.js";const a={},c={style:{padding:"3rem"}},r=e("h1",null,"Page Not Found",-1),d=e("p",null,[s(" Please return to "),e("a",{href:"/",style:{color:"darkcyan","text-decoration":"underline"}},"Home")],-1),_=[r,d];function l(i,u){return t(),n("div",c,_)}const h=o(a,[["render",l]]);export{h as default};
